646c4431c8f5934aa901b10850bd91914f7ccf34
mrsethparker <36980118+mrsethparker@users.noreply.github.com>
2018-04-03 19:07:16 -0700

    style: implement layout margins as dimen resources
    change layout margins from hardcoded values to dimension resources

--- app/src/main/java/com/example/android/coffeequiz/MainActivity.java
+++ app/src/main/java/com/example/android/coffeequiz/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
